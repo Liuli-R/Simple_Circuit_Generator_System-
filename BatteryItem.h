3a2921444863ca558aa986d92033cf584de25b9d
荣文焕 <3999422867@qq.com>
2026-06-16 17:49:14 +0800

    完善Battery的设计

--- BatteryItem.h
+++ BatteryItem.h
@@ -1,10 +1,15 @@
 #ifndef BATTERYITEM_H
 #define BATTERYITEM_H
 
-class BatteryItem
+#include "ComponentItem.h"
+
+class BatteryItem:public ComponentItem
 {
-public:
-    BatteryItem();
+    public:
+        BatteryItem(int BatteryId,QGraphicsItem *parent=nullptr);
+        void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
+        //重写接口绘制函数->作区分这是提供给QGraphicsItem的
+        //而有一个功能相似的PaintEvent是提供给QWidget的
 };
 
 #endif // BATTERYITEM_H
