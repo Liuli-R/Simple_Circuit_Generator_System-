e74738c36d6ac097b30894c99c3cfda7243fdc8f
荣文焕 <3999422867@qq.com>
2026-06-18 21:36:30 +0800

    去除了优化后不必要的文件

--- Circuit_code/CircuitAsciiRenderer.cpp
+++
@@ -1,6 +0,0 @@
-#include "CircuitAsciiRenderer.h"
-
-std::string CircuitAsciiRenderer::render(const Circuit& circuit) {
-    (void)circuit;
-    return "";
-}
--- Circuit_code/CircuitAsciiRenderer.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-#include "Circuit.h"
-
-class CircuitAsciiRenderer {
-public:
-    std::string render(const Circuit& circuit);
-};
